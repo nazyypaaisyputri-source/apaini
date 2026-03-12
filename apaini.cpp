ab0562171e0d7f450368da798a8b85415a4e0bbc
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-03-12 11:57:46 +0700

    Menambahkan perulangan untuk menginput elemen ke dalam array

--- apaini.cpp
+++ apaini.cpp
@@ -16,3 +16,13 @@ void input(){ //procedure input
             cout << "\nArray Tidak Boleh Lebih dari 20\n";
         }
     }
+    cout << endl;
+    cout << "================================";
+    cout << "=========Masukan Element Array=========";
+    cout << "================================";
+
+    for (int i = 0; i < n; i++){
+        cout << "Data ke-" << (i + 1) << " : ";
+        cin >> arr[i];
+    }
+}
